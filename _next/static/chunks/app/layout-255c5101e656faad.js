e7dcee975fa60183438f461fce259bce9115a6af
Pratik Sharma <47445734+pratik012@users.noreply.github.com>
2023-09-25 10:42:07 +0545

    nextjs app

---
+++ _next/static/chunks/app/layout-255c5101e656faad.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{6026:function(n,e,t){Promise.resolve().then(t.t.bind(t,7699,23)),Promise.resolve().then(t.t.bind(t,8877,23)),Promise.resolve().then(t.t.bind(t,4724,23))},8877:function(){},7699:function(){}},function(n){n.O(0,[724,971,864,744],function(){return n(n.s=6026)}),_N_E=n.O()}]);
